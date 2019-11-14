78236a1b5d7ef4006b1ff2a23f07869615beebd1
Miguel235711 <a01423629@itesm.mx>
2019-11-14 00:31:52 -0600

    addingSomeSkeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatPaginatorModule,
+  MatDialogModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { SignupComponent } from './signup/signup.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { PracticesComponent } from './practices/practices.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { MygroupsComponent } from './mygroups/mygroups.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    ForgotComponent,
+    SignupComponent,
+    NewsfeedComponent,
+    PracticesComponent,
+    InventoryComponent,
+    MygroupsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
